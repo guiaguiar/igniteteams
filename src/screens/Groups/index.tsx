868e44e6cb0c8eaf0e63c5083c606fd2cd023af0
Guilherme Aguiar <guilherme.aguiar@bitfy.app.com>
2023-04-24 10:27:50 -0300

    started to use yarn

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,20 @@
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 
-import * as S from './styles';
+import { Container } from './styles';
 
 export function Groups() {
   return (
-    <S.Container>
-    </S.Container>
+    <Container>
+      <Header />
+      <Highlight 
+        title="Turmas"
+        subtitle="jogue com sua turma"
+      />
+
+      <GroupCard 
+        title="Galera do Ignite" />
+    </Container>
   );
 }
